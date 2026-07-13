dcc974df10bd51734d28d9816c59fea84f4bd8f1
BELLprime <natthakitrodruean@gmail.com>
2026-07-14 01:15:26 +0700

    change attribute in html and Create querySelector and readForm() in main.js [have done for TODO:1-2 of 6]

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
-import './style.css';
-
 const form = document.querySelector('#request-form');
 
-
 // TODO 1: query preview/status/list elements
+const preview = {
+  previewName: document.querySelector('#preview-name'),
+  previewType: document.querySelector('#preview-type'),
+  previewDetails: document.querySelector('#preview-details'),
+}; 
+const statusElement = document.querySelector('#form-status');
+const requestList = document.querySelector('#request-list');
+const detailsCount = document.querySelector('#details-count');
+
 // TODO 2: readForm()
+function readForm() {
+    return Object.fromEntries(new FormData(form).entries());
+}
 // TODO 3: renderPreview(data)
+
 // TODO 4: validate(data)
+
 // TODO 5: renderErrors(errors)
+
 // TODO 6: input and submit listeners
+
 
 console.log('LAB 3 starter ready', form);
